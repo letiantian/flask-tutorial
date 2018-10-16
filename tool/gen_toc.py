227d7c1078ff93c1c1fb9eeffa6aa84ea490d1d5
letian <sunlt1699@gmail.com>
2018-10-16 09:19:39 +0800

    更新目录

---
+++ tool/gen_toc.py
@@ -0,0 +1,12 @@
+import os
+import urllib.parse
+
+ARTICLE = 'article'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+article_dir_path = os.path.join(dir_path, '..', ARTICLE)
+
+articles = [item for item in os.listdir(article_dir_path)]
+articles.sort()
+for file_name in articles:
+    # print(file_name)
+    print('* [{}]({})  '.format(file_name[0:-3], urllib.parse.quote(file_name)))
